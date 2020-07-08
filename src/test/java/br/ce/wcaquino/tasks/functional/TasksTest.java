fc9fd0fd8806a13da78cda39a4692d21909e7cc6
Reginaldo Lima <florentinolim@gmail.com>
2020-07-07 23:50:31 -0300

    paralelismo em docker

--- src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
+++ src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
@@ -1,5 +1,6 @@
 package br.ce.wcaquino.tasks.functional;
-
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
@@ -7,18 +8,22 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class TasksTest {
 	
-	public WebDriver acessarAplicacao() {
-		WebDriver driver = new ChromeDriver();
+	public WebDriver acessarAplicacao() throws MalformedURLException {
+		//WebDriver driver = new ChromeDriver();
+		DesiredCapabilities cap = DesiredCapabilities.chrome();
+		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.91.155:4444/wd/hub"),cap);
 		driver.navigate().to("http://192.168.91.156:8001/tasks");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
 	}
 	
 	@Test
-	public void deveSalvarTarefaComSucesso() {
+	public void deveSalvarTarefaComSucesso() throws MalformedURLException {
 		WebDriver driver = acessarAplicacao();
 		try {
 			
@@ -28,7 +33,7 @@ public class TasksTest {
 		driver.findElement(By.id("addTodo")).click();
 		
 		//Escrever a descrição
-		driver.findElement(By.id("task")).sendKeys("Teste via Selenium2");
+		driver.findElement(By.id("task")).sendKeys("Teste via Selenium3");
 		
 		//Escrever a data 
 		driver.findElement(By.id("dueDate")).sendKeys("10/10/2030");
@@ -47,12 +52,11 @@ public class TasksTest {
 	}
 	
 	@Test
-	public void naoDevedeveSalvarTarefaSemDescricao() {
+	public void naoDevedeveSalvarTarefaSemDescricao() throws MalformedURLException {
 		WebDriver driver = acessarAplicacao();
 		
 		try {
-			
-					
+							
 		//clicar em Add Todo
 		driver.findElement(By.id("addTodo")).click();
 		
@@ -72,7 +76,7 @@ public class TasksTest {
 	  }
 	}
 		@Test
-		public void naoDevedeveSalvarTarefaSemData() {
+		public void naoDevedeveSalvarTarefaSemData() throws MalformedURLException {
 			WebDriver driver = acessarAplicacao();
 			try {
 				
@@ -82,7 +86,7 @@ public class TasksTest {
 			driver.findElement(By.id("addTodo")).click();
 			
 			//Escrever a descrição
-			driver.findElement(By.id("task")).sendKeys("Teste via Selenium2");
+			driver.findElement(By.id("task")).sendKeys("Teste via Selenium3");
 			
 			//Escrever a data 
 			driver.findElement(By.id("dueDate")).sendKeys("10/10/2030");
@@ -100,7 +104,7 @@ public class TasksTest {
 	}
 		
 		@Test
-		public void deveSalvarTarefaComDataPassada() {
+		public void deveSalvarTarefaComDataPassada() throws MalformedURLException {
 			WebDriver driver = acessarAplicacao();
 			try {
 				
@@ -109,7 +113,7 @@ public class TasksTest {
 			driver.findElement(By.id("addTodo")).click();
 			
 			//Escrever a descrição
-			driver.findElement(By.id("task")).sendKeys("Teste via Selenium2");
+			driver.findElement(By.id("task")).sendKeys("Teste via Selenium3");
 			
 			//Escrever a data 
 			driver.findElement(By.id("dueDate")).sendKeys("10/10/2030");
